4b9db25e961b1f6e832b237891a50d9073078b65
YanaGum <71185015+YanaGum@users.noreply.github.com>
2020-11-13 17:56:01 +0200

    ...

---
+++ java.js
@@ -0,0 +1,15 @@
+// var index = 0;
+// var links = new Array (
+//        "http://javascript.ru/forum/images/ca_serenity/misc/logo.gif", 
+//        "https://www.cyberforum.ru/images/cyberforum_logo.jpg", 
+//        "http://www.webdeveloper.com/forum/images/webdev-logo2.gif"
+// ); 
+
+// function slide(v) {
+//  var element = document.getElementById("pic");
+//  index += 1 * v;
+
+//  if(index > links.length - 1) index = 0;
+//  else if(index < 0) index = links.length - 1;
+//  element.src = links[index];
+// }
